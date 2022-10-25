d43193c4778f84b4350d24878359ec7c73ded18d
Mohammad <moh.hossain98@gmail.com>
2022-10-25 15:33:32 -0400

    added text effect

--- src/About.js
+++ src/About.js
@@ -1,45 +1,40 @@
 import React, { useState, useEffect } from "react";
-import BlinkingCursor from "./BlinkingCursor";
-import Portrait from "./assets/portrait.png";
+// import BlinkingCursor from "./BlinkingCursor";
+// import Portrait from "./assets/portrait.png";
+import Typewriter from "typewriter-effect";
 import "./About.css";
 
 function About({ setPage }) {
-  setPage("About.js");
-
-  const [helloWorld, setHelloWorld] = useState("");
-  const [reachedEnd, setReachedEnd] = useState(false);
-  const text = [
-    "H",
-    "e",
-    "l",
-    "l",
-    "o",
-    ",",
-    " ",
-    "w",
-    "o",
-    "r",
-    "l",
-    "d",
-    "!",
-  ];
-
-  useEffect(() => {
-    let helloText = "";
-    for (let i in text) {
-      setTimeout(() => {
-        // helloText += text[i];
-        console.log(i);
-        // setHelloWorld(helloText);
-      }, 5000);
-    }
-  }, []);
-
   return (
     <div className="About">
-      <h1 className="hello-world">{helloWorld}</h1>
-      <img src={Portrait} height={400}></img>
-      <BlinkingCursor></BlinkingCursor>
+      <div className="hello-text">
+        <h1 className="hello-world">Hello, World!</h1>
+        <h1 className="hello-world"> Hello, World!</h1>
+      </div>
+      <div className="bio">
+        <h3>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("I am ")
+                .callFunction(() => {
+                  console.log("String typed out!");
+                })
+                .typeString("Mohammad Hossain ")
+                .deleteChars(17)
+                .typeString("a full-stack developer")
+                .pauseFor(2500)
+                .callFunction(() => {
+                  console.log("All strings were deleted");
+                })
+                .start();
+            }}
+            options={{
+              loop: true,
+            }}
+          />
+        </h3>
+      </div>
     </div>
   );
 }
